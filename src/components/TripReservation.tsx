a97aeb7298df110bf62dc36b75a7465959c6cd00
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-07-12 14:08:12 -0300

    feat: add max guests on trip reservation component

--- src/components/TripReservation.tsx
+++ src/components/TripReservation.tsx
@@ -1,17 +1,25 @@
 'use client'
 
+import { Trip } from '@prisma/client'
 import { Button } from './Button'
 import DatePicker from './DatePicker'
 import Input from './Input'
 
-export function TripReservation() {
+interface TripReservationProps {
+  trip: Trip
+}
+
+export function TripReservation({ trip }: TripReservationProps) {
   return (
-    <form className="w-full grid grid-cols-4 gap-3 pb-5 border-b border-light-gray">
+    <form className="w-full grid grid-cols-4 gap-3 pb-10 border-b border-light-gray">
       <DatePicker placeholderText="Data de início" onChange={() => {}} />
 
       <DatePicker placeholderText="Data final" onChange={() => {}} />
 
-      <Input type="number" placeholder="Hóspedes" />
+      <Input
+        type="number"
+        placeholder={`Número de hóspedes (máximo ${trip.maxGuests})`}
+      />
 
       <div className="col-span-full flex items-center justify-between">
         <span className="text-sm font-medium text-dark-purple">
